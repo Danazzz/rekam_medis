977b6dde7ddb0eb8b3b80fc46f32619404aa83b1
Danazzz <artaputra95@gmail.com>
2021-06-16 14:54:53 +0800

    membuat tampilan login

--- auth/login.php
+++ auth/login.php
@@ -8,10 +8,24 @@
     <meta name="author" content="">
     <title>Naditya Wisna &mdash; Login</title>
     <!-- Bootstrap Core CSS -->
-    <link href="../_assets/sidebar/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../_assets/simple-sidebar/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="wrapper">
+        <div class="container">
+            <center>
+                <form action ="" method="post" class="navbar-form">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input type="password" name="pass" class="form-control" required>
+                    </div>
+                </form>
+            </center>
+        </div>
 
     </div>
 </body>
