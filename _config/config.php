75bf6a5835b1cec28e29bc324d2bcbf9df57fba7
Danazzz <artaputra95@gmail.com>
2021-06-16 20:14:23 +0800

    tambah komen

--- _config/config.php
+++ _config/config.php
@@ -1,12 +1,16 @@
 <?php
+// setting default timezone 
 date_default_timezone_set('asia/hong_kong');
+
 session_start();
 
+// koneksi ke database
 $con=mysqli_connect('localhost','root','','rekam_medis');
 if(mysqli_connect_errno()){
     echo mysqli_connect_error();
 }
 
+// fungsi base_url
 function base_url($url = null){
     $base_url = "http://localhost/Klp3_ProjectWeb";
     if($url != null){
