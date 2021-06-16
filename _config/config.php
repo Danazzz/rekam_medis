222bf6a4ffd8f0776e08c8082ac8c9a517414c06
Danazzz <artaputra95@gmail.com>
2021-06-16 18:52:02 +0800

    koneksi ke database rekammedis

--- _config/config.php
+++ _config/config.php
@@ -1,3 +1,9 @@
 <?php
+date_default_timezone_set('asia/hong_kong');
+session_start();
 
+$con=mysqli_connect('localhost','root','','rekam_medis');
+if(mysqli_connect_errno()){
+    echo mysqli_connect_error();
+}
 ?>
