ea67f2aa83437a73d0dcc0eb9ea745a346f4be22
Danazzz <artaputra95@gmail.com>
2021-06-17 00:06:28 +0800

    Membuat Security Akses Page Login dan Dashboard

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-HOME <a href="auth/logout.php">Logout</a>
+<?php
+require_once "_config/config.php";
+if(isset($_SESSION['user'])){
+    echo "Homepage. <a href=\"auth/logout.php\">Logout</a>";
+}
+else{
+    echo "<script>window.location='".base_url('auth/login.php')."';</script>";
+}
+?>    
