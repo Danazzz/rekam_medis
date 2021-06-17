d7794bd719fd092286196ac3d8e31ad27d291079
Danazzz <artaputra95@gmail.com>
2021-06-18 01:42:10 +0800

    memperbaiki error 90%

--- obat/data.php
+++ obat/data.php
@@ -8,16 +8,16 @@
                 <a href = "" class="btn btn-default btn-xs"><i class= "glyphicon glyphicon-refresh"></i></a>
                 <a href = "" add.php="btn btn-success btn-xs"><i class= "glyphicon glyphicon-plus"></i>Tambah obat</a>
             </div> 
-        </h4>  
-        <div style="margin-bottom: 20px; ">
+        </h4>
+        <div style="margin-bottom: 20px;">
             <form class = "form-inline" action="" method="post">
                 <div class="form-group ">
                     <input type="text" name="pencarian" class="form-control" placeholder="pencarian "
-            </div>
-            <div class="form-group ">
-                <button type="submit" class="btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span><button > 
-            <div  
-         </form>
+                </div>
+                <div class="form-group ">
+                    <button type="submit" class="btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span><button > 
+                </div>
+            </form>
         </div>
         <div class = "table-responsive">
             <table class="table table-striped table-bordered table-hover">
@@ -47,9 +47,9 @@
                         $query = $sql;
                         $queryJml = $sql;
                     } else {
-                      $query = "SELECT * FROM tb_obat LIMIT $posisi, $batas";
-                      $queryJml = "SELECT * FROM tb_obat";
-                      $no = $posisi + 1;
+                        $query = "SELECT * FROM tb_obat LIMIT $posisi, $batas";
+                        $queryJml = "SELECT * FROM tb_obat";
+                        $no = $posisi + 1;
                     }
                 } else {
                     $query = "SELECT * FROM tb_obat LIMIT $posisi, $batas";
@@ -59,11 +59,11 @@
                 
                 $sql_obat = mysqli_query($con,$query) or die (mysqli_error($con));
                 if(mysqli_num_rows($sql_obat) > 0) {
-                    while($data = mysqli_fetch_array(sql_obat)) { ?>
+                    while($data = mysqli_fetch_array($sql_obat)) { ?>
                         <tr>
                             <td><?=$no++?>,</td>
-                            <td><?=data['nama_obat']?></td>
-                            <td><?=data['ket_obat']?></td>
+                            <td><?=$data['nama_obat']?></td>
+                            <td><?=$data['ket_obat']?></td>
                             <td class = "text-center">
                                 <a href = "edit.php?id=<?=$data['id_obat']?>" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-edit"></i></a>
                                 <a href = "del.php?id=<?=$data['id_obat']?>" onclick = "return confirm('Yakin akan menghapus data?')" class= "btn btn-danger btn-xs"><i class="glyphicon glyphicon-trash"></i></a>
@@ -79,18 +79,18 @@
             </table>
         </div>
         <?php
-        if($_POST['pencarian'] == ''){ ? 
+        if($_POST['pencarian'] == '') { ?> 
             <div style="float:left;">
                 <?php 
                 $jml = mysqli_num_rows(mysqli_query($con, $queryJml));
                 echo "Jumlah Data : <b>$jml</b>";
                 ?>
-             </div>
-             <div style="float:right;">
+            </div>
+            <div style="float:right;">
                 <ul class="pagination pagination-sm" style="margin:0">
                     <?php
                     $jml_hal = ceil($jml / $batas);
-                    for ($i=1; $i <= $jml_hal; $i**) {
+                    for ($i=1; $i <= $jml_hal; $i++) {
                         if($i != $hal) {
                             echo "<li><a href=\"?hal=$i\">$i</a></li>";
                         } else {
@@ -99,13 +99,13 @@
                     }
                     ?>
                 </ul>
-             </div>
+            </div>
             <?php
-        }    else { 
-                echo "<div style=\"float:left;\">";
-                $jml = mysqli_num_rows(mysql i_query)$con, $queryJml));
-                echo "Data Hasil Pencarian : <b>$jml</b>";
-                echo "</div";
+        } else { 
+            echo "<div style=\"float:left;\">";
+            $jml = mysqli_num_rows(mysqli_query($con, $queryJml));
+            echo "Data Hasil Pencarian : <b>$jml</b>";
+            echo "</div";
         }
         ?> 
     </div>
