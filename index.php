196e4337643260d9ae53d9f07c07839ed0272442
cintyawardani <cintyawardanii@gmail.com>
2021-06-16 21:07:37 -0700

    dikarenakan ada session ubah auth menjadi dashboard

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once "_config/config.php";
 if(isset($_SESSION['user'])){
-    echo "Homepage. <a href=\"auth/logout.php\">Logout</a>";
+    echo "<script>window.location='".base_url(dashboard.php')."';</script>";
 }
 else{
     echo "<script>window.location='".base_url('auth/login.php')."';</script>";
