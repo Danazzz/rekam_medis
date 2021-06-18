deed34eafddb1aa0bce2e642c4de8a985f6717be
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-18 13:57:36 +0800

    Menambah div class seperti pada data.php

--- obat/generate.php
+++ obat/generate.php
@@ -1,3 +1,12 @@
 <?php include_once('../_header.php'); ?>
 
+    <div class="box">
+		<h1>Obat</h1>
+		<h4>
+			<div class="pull-right">
+				<a href="" class="btn btn-default btn-xs"><i class="glyphicon glyphicon-refresh"></i></a>
+				<a href="generate.php" class="btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i> Tambah Obat</a>
+			</div>
+		</h4>
+
 <?php include_once('../_footer.php'); ?>
