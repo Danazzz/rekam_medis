97a0454f261d026e4a2136e6891b67c6918218d4
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-18 13:58:06 +0800

    Menghapus href ke generate.php

--- obat/generate.php
+++ obat/generate.php
@@ -5,7 +5,6 @@
 		<h4>
 			<div class="pull-right">
 				<a href="" class="btn btn-default btn-xs"><i class="glyphicon glyphicon-refresh"></i></a>
-				<a href="generate.php" class="btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i> Tambah Obat</a>
 			</div>
 		</h4>
 
