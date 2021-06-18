b87af777efe3496fc61bdd44c81eacabf8380db8
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-18 14:30:05 +0800

    menambah div class baru

--- obat/generate.php
+++ obat/generate.php
@@ -1,11 +1,14 @@
 <?php include_once('../_header.php'); ?>
 
-    <div class="box">
+<div class="box">
 		<h1>Obat</h1>
 		<h4>
 			<div class="pull-right">
 				<a href="data.php" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-chevron-left"></i> Kembali</a>
 			</div>
 		</h4>
-
+    <div class="row">
+        
+    </div>
+</div>
 <?php include_once('../_footer.php'); ?>
