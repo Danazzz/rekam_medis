76a4e2e4b72c64a98156d7fb3d529134a66c9acb
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-18 23:07:20 +0800

    mengganti arahan form action ke proses.php

--- obat/add.php
+++ obat/add.php
@@ -11,14 +11,8 @@
 		    </h4>
         <div class="row">
             <div class="col-lg-6 col-lg-offset-3">
-                <form action="add.php" method="post">
-                    <div class="form-group">
-                        <label for="count_add">Banyak record yang akan ditambahkan</label>
-                        <input type="text" name="count_add" id="count_add" maxlength="2" pattern="[0-9]+" class="form-control" required>
-                    </div>
-                    <div class="form-group pull-right">
-                        <input type="submit" name="generate" value="Generate" class="btn btn-">
-                    </div>
+                <form action="proses.php" method="post">
+                    
                 </form>
             </div>
         </div>
