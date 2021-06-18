5d89e9c72f472f70db436fc9728c95881fb10cf2
Danazzz <artaputra95@gmail.com>
2021-06-19 01:29:37 +0800

    memperbaiki keliruan di line 20 dan juga menambahkan error report utk database

--- obat/edit.php
+++ obat/edit.php
@@ -11,14 +11,13 @@
                 <div class="col-lg-6 col-lg-offset-3">
                     <?php
                     $id = @$_GET['id'];
-                    $sql_obat = mysqli_query($con, "SELECT * FROM tb_obat WHERE id_obat = '$id'");
+                    $sql_obat = mysqli_query($con, "SELECT * FROM tb_obat WHERE id_obat = '$id'") or die (mysqli_error($con));
                     $data = mysqli_fetch_array($sql_obat);
                     ?>
-
                     <form action="proses.php" method="post">
                         <div class="form-group">
                             <label for="nama">Nama Obat</label>
-                            <input type="hidden" name="id" value="<?=$data['id_obat']?>"
+                            <input type="hidden" name="id" value="<?=$data['id_obat']?>">
                             <input type="text" name="nama" id="nama" value="<?=$data['nama_obat']?>" class="form-control" required autofocus>
                         </div>
                         <div class="form-group">
