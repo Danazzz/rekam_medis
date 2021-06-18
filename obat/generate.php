61ffb22bf29f9f73ab9fd8a17a2c34b22eff4fa3
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-18 14:12:30 +0800

    Mengarahkan href ke data.php

--- obat/generate.php
+++ obat/generate.php
@@ -4,7 +4,7 @@
 		<h1>Obat</h1>
 		<h4>
 			<div class="pull-right">
-				<a href="" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-chevron-left"></i> Kembali</a>
+				<a href="data.php" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-chevron-left"></i> Kembali</a>
 			</div>
 		</h4>
 
