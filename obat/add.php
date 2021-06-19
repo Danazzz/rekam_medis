56db87417a83d41a8ffcd577ba7a3d3686ade8d2
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-19 17:10:50 +0800

    replace dari main

--- obat/add.php
+++ obat/add.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<?php include_once('../_header.php'); ?>
-
-    <div class="box">
-		    <h1>Obat</h1>
-		    <h4>
-                <small>Tambah Data Poliklinik</small>
-			    <div class="pull-right">
-				    <a href="data.php" class="btn btn-info btn-xs"> Data</a>
-                    <a href="generate.php" class="btn btn-primary btn-xs"> Tembah Data</a>
-			    </div>
-		    </h4>
-        <div class="row">
-            <div class="col-lg-6 col-lg-offset-3">
-                <form action="proses.php" method="post">
-                    
-                </form>
-            </div>
-        </div>
-    </div>
-<?php include_once('../_footer.php'); ?>
-=======
 <?php include_once('../_header.php');?>
     <div class="box">
         <h1>Obat</h1>
@@ -49,4 +27,3 @@
     </div>
 
 <?php include_once('../_footer.php');?>
->>>>>>> 28dc788db59301fd12fc1664b6f1fb037ab84d99
