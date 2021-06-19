e3a3335efe8ff3e3eee6fa28c69c935283bd33e4
Danazzz <artaputra95@gmail.com>
2021-06-20 01:38:45 +0800

    Memperbaiki error 80%

--- dokter/edit.php
+++ dokter/edit.php
@@ -1,43 +1,47 @@
-<?php include_once('../_header.php');?>
-    <div class="box">
-        <h1>Dokter</h1>
-            <h4>
-                <small>Edit Data Dokter</small>
-                <div class="pull-right">
-                    <a href="data.php" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-chevron-left"></i>Kembali</a>
-                </div>
-            </h4>
-            <div class="row">
-                <div class="col-lg-6 col-lg-offset-3">
-                    <?php
-                    $id = @$_GET['id'];
-                    $sql_dokter = mysqli_query($con, "SELECT * FROM tb_dokter WHERE id_dokter = '$id'") or die (mysqli_error($con));
-                    $data = mysqli_fetch_array($sql_dokter);
-                    ?>
-                    <form action="proses.php" method="post">
-                        <div class="form-group">
-                            <label for="nama">Nama Dokter</label>
-                            <input type="hidden" name="id" value="<?=$data['id_dokter']?>">
-                            <input type="text" name="nama" id="nama" value="<?=$data['nama_dokter']?>" class="form-control" required autofocus>
-                        </div>
-                        <div class="form-group">
-                            <label for="nama">spesialis</label>
-                            <textarea name="spesialis" id="spesialis" class="form-control" required><?=$data['spesialis']?></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="nama">Alamat</label>
-                            <textarea name="Alamat" id="Alamat" class="form-control" required><?=$data['Alamat']?></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="nama">no_tlp</label>
-                            <textarea name="no_tlp" id="no_tlp" class="form-control" required><?=$data['no_tlp']?></textarea>
-                        </div>
-                        <div class="form-group pull-right">
-                            <input type="submit" name="edit" value="Simpan" class="btn btn-success">
-                        </div>
-                    </form>
-                </div>
-            </div>
-    </div>
+<?php
+include_once('../_header.php');
+?>
 
-<?php include_once('../_footer.php');?>
+<div class="box">
+	<h1>Dokter</h1>
+	<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+	<h4>
+		<small>Edit Data Dokter</small>
+		<div class="pull-right">
+			<a href="data.php" class="btn btn-warning btn-xs"><i class="glyphicon glyphicon-chevron-left"></i> Kembali</a>
+		</div>
+	</h4>
+	<div class="row">
+		<div class="col-lg-6 col-lg-offset-3">
+			<?php
+			$id = @$_GET['id'];
+			$sql_dokter = mysqli_query($con, "SELECT * FROM tb_dokter WHERE id_dokter='$id'") or die(mysqli_error($con));
+			$data = mysqli_fetch_array($sql_dokter);
+			?>
+			<form action="proses.php" method="post">
+				<div class="form-group">
+					<label for="nama">Nama Dokter</label>
+					<input type="hidden" name="id" value="<?= $data['id_dokter'] ?>">
+					<input type="text" name="nama" id="nama" value="<?= $data['nama_dokter'] ?>" class="form-control" required="" autofocus="">
+				</div>
+				<div class="form-group">
+					<label for="spesialis">Spesialis</label>
+					<input type="text" name="spesialis" id="spesialis" value="<?= $data['spesialis'] ?>" class="form-control" required="">
+				</div>
+				<div class="form-group">
+					<label for="alamat">Alamat</label>
+					<textarea name="alamat" id="alamat" class="form-control" required=""><?= $data['alamat'] ?></textarea>
+				</div>
+				<div class="form-group">
+					<label for="telp">No. Telepon</label>
+					<input type="number" name="telp" id="telp" value="<?= $data['no_telp'] ?>" class="form-control" required="">
+				</div>
+				<div class="form-group">
+					<input type="submit" name="edit" value="Simpan" class="btn btn-success">
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<?php include_once('../_footer.php'); ?>
