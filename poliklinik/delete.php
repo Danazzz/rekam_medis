721d716c885dc17e5c4bdca2b8c7e90d1d5bc7fc
Kadekk01 <dekyudiarsana26@gmail.com>
2021-06-20 15:53:46 +0800

    Mengganti elemen obat ke poliklinik sampai databasenya

--- poliklinik/delete.php
+++ poliklinik/delete.php
@@ -2,6 +2,6 @@
 require_once "../_config/config.php";
 require "../_assets/libs/vendor/autoload.php";
 
-mysqli_query($con, "DELETE FROM tb_obat WHERE id_obat = '$_GET[id]'") or die (mysqli_error($con));
+mysqli_query($con, "DELETE FROM tb_poliklinik WHERE id_poli = '$_GET[id]'") or die (mysqli_error($con));
 echo "<script>window.location='data.php';</script>";
 ?>
