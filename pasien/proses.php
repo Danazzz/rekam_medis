3bb380acbba38552fb3d1a32ccfd96d677a943a8
billatorya <bilavictory@gmail.com>
2021-06-21 03:04:44 +0800

    Membuat validasi data agar tidak terduplicate

--- pasien/proses.php
+++ pasien/proses.php
@@ -7,22 +7,39 @@ use Ramsey\Uuid\Excepetion\UnsatisfiedDependencyException;
 
 if(isset($_POST['add'])) {
     $uuid = Uuid::uuid4()->toString();
-    $nomor = trim(mysqli_real_escape_string($con, $_POST['nomor']));
+    $identitas = trim(mysqli_real_escape_string($con, $_POST['identitas']));
     $nama = trim(mysqli_real_escape_string($con, $_POST['nama']));
-    $jenis = trim(mysqli_real_escape_string($con, $_POST['jenis']));
+    $jk = trim(mysqli_real_escape_string($con, $_POST['jk']));
     $alamat = trim(mysqli_real_escape_string($con, $_POST['alamat']));
-    $no_telp = trim(mysqli_real_escape_string($con, $_POST['no_telp']));
-    mysqli_query($con,"INSERT INTO tb_pasien(id_pasien, nomor_identitas, nama_pasien, jenis_kelamin, alamat, no_telp ) VALUES ('$uuid', '$nomor', '$nama', '$jenis', '$alamat', '$no_telp') or die (mysqli_error($con));
-    echo "<script>Window.location='data.php';</script>";
+    $telp = trim(mysqli_real_escape_string($con, $_POST['telp']));
+    // mengecek duplikasi saat sedang menambahkan data/inputan baru
+    $sql_check_identitas = mysqli_query($con, "SELECT * FROM tb_pasien WHERE nomor_identitas='$identitas'") or die(mysqli_error($con));
+    // jika data sudah ada maka akan muncul alert
+        if(mysqli_num_rows($sql_check_identitas) > 0){
+            echo "<script>alert('Nomor identitas sudah pernah diinput!');window.location='add.php?id=$id';</script>";
+        }
+        else{
+            mysqli_query($con,"INSERT INTO tb_pasien(id_pasien, nomor_identitas, nama_pasien, jenis_kelamin, alamat, no_telp) 
+                                VALUES ('$uuid', '$identitas', '$nama', '$jk', '$alamat', '$telp')") or die (mysqli_error($con));
+            echo "<script> alert('Data berhasil ditambah');window.location='data.php';</script>";
+        }
 }
 else if(isset($_POST['edit'])) {
     $id = $_POST['id'];
-    $nomor = trim(mysqli_real_escape_string($con, $_POST['nomor']));
+    $identitas = trim(mysqli_real_escape_string($con, $_POST['identitas']));
     $nama = trim(mysqli_real_escape_string($con, $_POST['nama']));
-    $jenis = trim(mysqli_real_escape_string($con, $_POST['jenis']));
+    $jk = trim(mysqli_real_escape_string($con, $_POST['jk']));
     $alamat = trim(mysqli_real_escape_string($con, $_POST['alamat']));
-    $no_telp = trim(mysqli_real_escape_string($con, $_POST['no_telp']));
-    mysqli_query($con,"UPDATE tb_pasien SET nomor_identitas = '$nomor', nama_pasien = '$nama' , jenis_kelamin = '$jenis', alamat = '$alamat', no_telp = '$no_telp' WHERE id_pasien = '$id'") or die (mysqli_error($con));
-    echo "<script>window.location='data.php';</script>";
+    $telp = trim(mysqli_real_escape_string($con, $_POST['telp']));
+    // mengecek duplikasi saat sedang mengubah data
+    $sql_check_identitas = mysql_query($con, "SELECT * FROM tb_pasien WHERE nomor_identitas = '$identitas' AND id_pasien != '$id'") or die(mysqli_error($con));
+    // jika data sudah ada maka akan muncul alert
+        if(mysqli_num_rows($sql_check_identitas) > 0){
+            echo "<script>alert('Nomor identitas sudah pernah diinput!');window.location='edit.php?id=$id';</script>";
+        }
+        else{
+            mysqli_query($con,"UPDATE tb_pasien SET nomor_identitas = '$identitas', nama_pasien = '$nama_pasien' , jenis_kelamin = '$jk', alamat = '$alamat', no_telp = '$telp' WHERE id_pasien = '$id'") or die (mysqli_error($con));
+            echo "<script> alert('Data berhasil diubah');window.location='data.php';</script>";
+        }
 }
 ?>
